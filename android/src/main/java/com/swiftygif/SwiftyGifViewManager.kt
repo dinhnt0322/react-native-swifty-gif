d08e0e4779f3dcd31538d20ca0f3f702f7611601
dinhnt0322 <71317074+dinhnt0322@users.noreply.github.com>
2024-08-24 22:02:54 +0700

    add some props for android

--- android/src/main/java/com/swiftygif/SwiftyGifViewManager.kt
+++ android/src/main/java/com/swiftygif/SwiftyGifViewManager.kt
@@ -1,6 +1,5 @@
 package com.swiftygif
 
-import android.graphics.Color
 import android.graphics.drawable.Animatable
 import android.util.Log
 import android.view.View
@@ -12,12 +11,16 @@ import com.facebook.drawee.drawable.ScalingUtils
 import com.facebook.drawee.interfaces.DraweeController
 import com.facebook.drawee.view.SimpleDraweeView
 import com.facebook.imagepipeline.image.ImageInfo
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.WritableMap
 import com.facebook.react.uimanager.ReactStylesDiffMap
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
-import com.facebook.react.uimanager.ViewProps
 import com.facebook.react.uimanager.annotations.ReactProp
+import com.facebook.react.uimanager.events.RCTEventEmitter
+import com.facebook.react.common.MapBuilder
 
 class SwiftyGifViewManager : SimpleViewManager<FrameLayout>() {
   override fun getName() = "SwiftyGifView"
@@ -26,10 +29,8 @@ class SwiftyGifViewManager : SimpleViewManager<FrameLayout>() {
     Fresco.initialize(reactContext)
     val frameLayout = FrameLayout(reactContext)
     val draweeView = SimpleDraweeView(reactContext)
-    val progressBar = ProgressBar(reactContext)
-    progressBar.visibility = View.GONE
+    sendEvent(frameLayout, EVENT_ON_LOAD_START, null)
     frameLayout.addView(draweeView)
-    frameLayout.addView(progressBar)
     return frameLayout
   }
 
@@ -40,31 +41,40 @@ class SwiftyGifViewManager : SimpleViewManager<FrameLayout>() {
 
   @ReactProp(name = "source")
   fun setGifUrl(view: FrameLayout, url: String) {
+    sendEvent(view, EVENT_ON_LOAD_START, null)
     val draweeView = view.getChildAt(0) as SimpleDraweeView
-    val progressBar = view.getChildAt(1) as ProgressBar
-    progressBar.visibility = View.VISIBLE
-    val controller: DraweeController = Fresco.newDraweeControllerBuilder()
-            .setUri(url)
-            .setControllerListener(object : BaseControllerListener<ImageInfo>() {
-              override fun onFinalImageSet(
-                      id: String?,
-                      imageInfo: ImageInfo?,
-                      animatable: Animatable?
-              ) {
-                Log.d("SwiftyGifViewManager", "GIF loaded successfully")
-                progressBar.visibility = View.GONE
-                if (view.getTag(R.id.paused) != true) {
-                  animatable?.start()
-                }
-              }
+    draweeView.controller = getDraweeController(url, view)
+  }
 
-              override fun onFailure(id: String?, throwable: Throwable?) {
-                progressBar.visibility = View.GONE
-                // Handle failure
-              }
-            })
-            .build()
-    draweeView.controller = controller
+  private fun getDraweeController(url: String, view: FrameLayout): DraweeController {
+    val controller: DraweeController = Fresco.newDraweeControllerBuilder()
+      .setUri(url)
+      .setControllerListener(object : BaseControllerListener<ImageInfo>() {
+        override fun onFinalImageSet(
+          id: String?,
+          imageInfo: ImageInfo?,
+          animatable: Animatable?
+        ) {
+          Log.d("SwiftyGifViewManager", "GIF loaded successfully")
+          sendEvent(view, EVENT_ON_LOAD_GIF_END, null)
+          if (view.getTag(R.id.paused) != true) {
+            animatable?.start()
+          }
+        }
+
+        override fun onFailure(id: String?, throwable: Throwable?) {
+          sendEvent(
+            view,
+            EVENT_ON_LOAD_GIF_ERROR,
+            Arguments.createMap().apply {
+              putString("error", throwable?.message)
+            }
+          )
+          // Handle failure
+        }
+      })
+      .build()
+    return controller
   }
 
   @ReactProp(name = "paused")
@@ -99,6 +109,32 @@ class SwiftyGifViewManager : SimpleViewManager<FrameLayout>() {
 
   private fun applyStyles(view: FrameLayout, props: ReactStylesDiffMap) {
 
-    // Handle other style properties as needed
+  }
+
+  override fun getExportedCustomDirectEventTypeConstants(): Map<String?, Any?>? {
+    return createExportedCustomDirectEventTypeConstants()
+  }
+
+  private fun createExportedCustomDirectEventTypeConstants(): Map<String?, Any?>? {
+    return MapBuilder.builder<String?, Any?>()
+      .put(EVENT_ON_LOAD_GIF_END, MapBuilder.of("registrationName", EVENT_ON_LOAD_GIF_END))
+      .put(EVENT_ON_LOAD_GIF_ERROR, MapBuilder.of("registrationName", EVENT_ON_LOAD_GIF_ERROR))
+      .put(EVENT_ON_LOAD_START, MapBuilder.of("registrationName", EVENT_ON_LOAD_START))
+      .build()
+  }
+
+  companion object {
+    private const val COMMAND_SET_TEXT = "setText"
+    private const val COMMAND_SET_TEXT_ID = 1
+
+    const val EVENT_ON_LOAD_GIF_END = "onLoadGifEnd"
+    const val EVENT_ON_LOAD_GIF_ERROR = "onLoadGifError"
+    const val EVENT_ON_LOAD_START = "onLoadGifStart"
+  }
+
+  private fun sendEvent(view: FrameLayout, eventName: String, params: WritableMap?) {
+    val reactContext = view.context as ReactContext
+    reactContext.getJSModule(RCTEventEmitter::class.java)
+      .receiveEvent(view.id, eventName, params)
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
-import {ImageSourcePropType, requireNativeComponent} from 'react-native';
-import React, {FC} from 'react';
+import {
+  ActivityIndicator,
+  ImageSourcePropType,
+  NativeSyntheticEvent,
+  requireNativeComponent,
+  StyleSheet,
+  View,
+} from 'react-native';
+import React, {FC, useMemo} from 'react';
 import {Image, ViewProps} from 'react-native';
 
 const SwiftyGifView = requireNativeComponent('SwiftyGifView');
 
 const resolveAssetSource = Image.resolveAssetSource;
 
-type SwiftyGifProps = {
+export type GifLoadErrorEvent = NativeSyntheticEvent<{error: string}>;
+
+export type SwiftyGifProps = ViewProps & {
   source: ImageSourcePropType;
   resizeMode?: string;
   paused?: boolean;
-} & ViewProps;
+  onLoadGifEnd?: () => void;
+  onLoadGifError?: (event: GifLoadErrorEvent) => void;
+  LoadingView?: React.ReactNode;
+};
 
 const SwiftyGif: FC<SwiftyGifProps> = props => {
-  const {source, paused = false} = props;
+  const {source, paused = false, onLoadGifEnd, onLoadGifError, style} = props;
+  const [isLoading, setIsLoading] = React.useState(true);
   let resolvedSource = '';
 
   if (source?.uri) {
@@ -25,7 +38,62 @@ const SwiftyGif: FC<SwiftyGifProps> = props => {
     resolvedSource = source;
   }
 
-  return <SwiftyGifView {...props} paused={paused} source={resolvedSource} />;
+  const handLoadGifEnd = () => {
+    setIsLoading(false);
+    onLoadGifEnd && onLoadGifEnd();
+  };
+
+  const handleLoadGifError = (event: GifLoadErrorEvent) => {
+    setIsLoading(false);
+    onLoadGifError && onLoadGifError(event);
+  };
+
+  const renderIndicator = useMemo(() => {
+    if (!isLoading) {
+      return null;
+    }
+    const defaultIndicatorSize = style
+      ? Math.min(style?.width ?? 0, style?.height ?? 0) / 2
+      : 'small';
+    return (
+      <View style={[style, styles.indicator]}>
+        {props.LoadingView ? (
+          props.LoadingView
+        ) : (
+          <ActivityIndicator size={defaultIndicatorSize} />
+        )}
+      </View>
+    );
+  }, [isLoading, props.LoadingView, style]);
+
+  const onLoadGifStart = () => {
+    setIsLoading(true);
+  };
+
+  return (
+    <View style={[style, styles.container]}>
+      <SwiftyGifView
+        {...props}
+        paused={paused}
+        source={resolvedSource}
+        onLoadGifStart={onLoadGifStart}
+        onLoadGifEnd={handLoadGifEnd}
+        onLoadGifError={handleLoadGifError}
+      />
+      {renderIndicator}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  indicator: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+  },
+  container: {
+    overflow: 'hidden',
+  },
+});
 
 export default SwiftyGif;
